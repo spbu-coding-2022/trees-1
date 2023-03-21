71f82aea48b37c9d9c9d4aabd21b1e996aa61e32
Georgy S <it.is.mr.lag@gmail.com>
2023-03-22 01:52:12 +0300

    fixe: Fixe problems with nullable fields in Nodes

--- lib/src/main/kotlin/Tree/RBtree/RBBalancer.kt
+++ lib/src/main/kotlin/Tree/RBtree/RBBalancer.kt
@@ -4,7 +4,7 @@ import AVLtree.AVLNode
 import AbstractTree.Weighted.BalancerParent
 
 class RBBalancer<Pack: Comparable<Pack>>: BalancerParent<Pack, RBNode<Pack>>() {
-    override fun balance(node: RBNode<Pack>): RBNode<Pack>{
+    override fun balance(node: RBNode<Pack>): RBNode<Pack> {
         TODO("Not yet implemented")
     }
 }
