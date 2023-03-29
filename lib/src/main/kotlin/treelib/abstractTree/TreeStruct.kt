b0f9acfce37fd1578d069616b6ad613830af1bdb
Georgy S <it.is.mr.lag@gmail.com>
2023-03-29 18:48:59 +0300

    fix: Add null-safety checks; Abstract deleteItem and insertItem

--- lib/src/main/kotlin/treelib/abstractTree/TreeStruct.kt
+++ lib/src/main/kotlin/treelib/abstractTree/TreeStruct.kt
@@ -1,22 +1,25 @@
 package treelib.abstractTree
 
+import treelib.binTree.BINNode
+
 abstract class TreeStruct<Pack : Comparable<Pack>, NodeType : Node<Pack, NodeType>> {
 
     protected abstract var root: NodeType?
 
     protected fun getLeafForInsert(item: Pack): NodeType? {
-        //TODO test - getParentByValue
-        var currentNode = root
+        //TODO test - getParentByValue; [не нравится, что currentNode = it.right не требует проверки на null]
+        var currentNode: NodeType? = root ?: return null
+
         while (true) {
-            if (currentNode != null) {
-                if (item > currentNode.value) {
-                    if (currentNode.right != null) currentNode = currentNode.right
-                    else return currentNode
+            currentNode?.let {
+                if (item > it.value) {
+                    if (it.right == null) return currentNode
+                    currentNode = it.right
                 } else {
-                    if (currentNode.left != null) currentNode = currentNode.left
-                    else return currentNode
+                    if (it.left == null) return currentNode
+                    currentNode = it.left
                 }
-            } else return null
+            } ?: throw Exception("Impossible case or multithreading problem ")
         }
     }
 
@@ -29,101 +32,154 @@ abstract class TreeStruct<Pack : Comparable<Pack>, NodeType : Node<Pack, NodeTyp
         */
 
         var currentNode = root
-        if (findItem(item) == null) return null // (2)
+        if (findItem(item) == null) throw Exception("getParentByValue shouldn't be used with a value doesn't exist in the tree")// (2)
 
         if ((currentNode != null) && (currentNode.value == item)) return null
 
         while (true) {
-            if (currentNode != null) {
+            currentNode?.let {
                 when {
-                    currentNode.right != null -> if (currentNode.right!!.value == item) return currentNode
-                    currentNode.left != null -> if (currentNode.left!!.value == item) return currentNode
-                }
-                if (item > currentNode.value) currentNode = currentNode.right
-                else currentNode = currentNode.left
+                    it.right != null -> {
+                        if (it.right?.value == item) return currentNode
+                        else throw Exception("impossible case, or multithreading threads problem")
+                    }
 
-            } else return null // (1)
+                    it.left != null -> {
+                        if (it.left?.value == item) return currentNode
+                        else throw Exception("impossible case, or multithreading threads problem")
+                    }
+                }
+                if (item > it.value) currentNode = it.right
+                else currentNode = it.left
+
+            } ?: throw Exception("getParentByValue shouldn't be used with 'root == null'")// (1)l ->
         }
     }
 
     protected infix fun Pack.inRightOf(node: NodeType?): Boolean {
         //TODO - test inInRightOf
-        if ((node != null) && (node.right != null)) {
-            if (node.right!!.value == this) return true
+        if ((node == null) || (node.right == null)) return false
+        node.right?.let {
+            if (it.value == node.value) return true
         }
         return false
     }
 
     protected infix fun Pack.inLeftOf(node: NodeType?): Boolean {
         //TODO - test inInLeftOf
-        if ((node != null) && (node.left != null)) {
-            if (node.left!!.value == this) return true
+        if ((node == null) || (node.left == null)) return false
+        node.left?.let {
+            if (it.value == this) return true
         }
         return false
     }
 
     private fun getRightMinNode(localRoot: NodeType): NodeType? {
+        /* null - means NodeType.right doesn't exist, another variant impossible */
         //TODO test - getLeafForDelete [проверить, что нет проблем с (->1)]
-        /*
-        null - means NodeType.right doesn't exist, another variant impossible
-        */
-        var currentNode: NodeType?
-        if (localRoot.right != null) currentNode = localRoot.right
-        else return null
+        var currentNode: NodeType? = null
+
+        localRoot.right?.let {
+            currentNode = localRoot.right
+        } ?: return null
 
         while (true) {
-            if (currentNode != null) {
-                if (currentNode.left == null) return currentNode
-                else currentNode = currentNode.left
-            } else return null //(->1)
+            currentNode?.let { curNode ->
+                if (curNode.left == null) return currentNode
+                else currentNode = curNode.left
+            } ?: throw Exception("impossible case, or multithreading threads problem") //(->1)
         }
     }
 
     private fun getLeftMaxNode(localRoot: NodeType): NodeType? {
+        /* null - means NodeType.left doesn't exist, another variant impossible */
         //TODO test - getLeafLeftMax [проверить, что нет проблем с (->1)]
-        /*
-        null - means NodeType.left doesn't exist, another variant impossible
-        */
-        var currentNode: NodeType?
-        if (localRoot.left != null) currentNode = localRoot.left
-        else return null
+        var currentNode: NodeType? = null
+
+        localRoot.left?.let {
+            currentNode = localRoot.left
+        } ?: return null
 
         while (true) {
-            if (currentNode != null) {
-                if (currentNode.right == null) return currentNode
-                else currentNode = currentNode.right
-            } else return null //(->1)
+            currentNode?.let { curNode ->
+                if (curNode.right == null) return currentNode
+                else currentNode = curNode.right
+            } ?: throw Exception("impossible case, or multithreading threads problem") //(->1)
         }
     }
 
     protected fun getNodeForReplace(localRoot: NodeType?): NodeType? {
         /* Behaviour: null - localRoot doesn't have children */
-        if (localRoot != null) {
-            val replaceNode = getRightMinNode(localRoot)
+        localRoot?.let {
+            val replaceNode = getRightMinNode(it)
             if (replaceNode != null) return replaceNode
-            else return getLeftMaxNode(localRoot)
-        } else return null
+            else return getLeftMaxNode(it)
+        } ?: return null
     }
 
     protected fun findItem(obj: Pack): NodeType? {
         var currentNode = root
-        if (root == null) {
-            return null
-        }
+        if (root == null) return null
+
         while (true) {
             if (obj == currentNode?.value) return currentNode
             else {
-                if (currentNode != null) {
-                    if (obj > currentNode.value) currentNode = currentNode.right
-                    else currentNode = currentNode.left
-                } else return null
+                currentNode?.let {
+                    if (obj > it.value) currentNode = it.right
+                    else currentNode = it.left
+                } ?: return null
             }
         }
     }
 
-    protected abstract fun insertItem(item: Pack): NodeType?
+    protected fun insertItem(item: Pack): NodeType {
+        val parentNode: NodeType?
+        val currentNode: NodeType
+        val updateNode: NodeType? = findItem(item)
 
-    protected abstract fun deleteItem(item: Pack): NodeType?
+        if (updateNode == null) {
+            parentNode = getLeafForInsert(item)
+            currentNode = createNode(item)
+
+            linkNewNode(currentNode, parentNode)
+
+            return currentNode
+        }
+        updateNode.value = item
+        return updateNode
+    }
+
+    protected fun deleteItem(item: Pack): NodeType? {
+        /*TODO: test - deleteItem [невозможные ветки: - проверить, что они реально невозможны
+           (->1 & 2) => что невозможно достичь deleteNode = parentNode?. со значением null (потому что inRightOf/inLeftOf)
+           (->3) => странный кейс, когда удаляемое значение - null; такого вообще быть не должно
+           ] */
+        val parentNode: NodeType? // parent node of the node for deleting
+        val deleteNode: NodeType?  // node for deleting
+        var replaceNode: NodeType?   // node for relink on the place deleted node
+
+        if (findItem(item) != null) return null
+
+        parentNode = getParentByValue(item)
+        if (parentNode != null) {
+            deleteNode = when {
+                item inRightOf parentNode -> parentNode.right //(->1)
+                item inLeftOf parentNode -> parentNode.left   //(->2)
+                else -> null
+            }
+        } else deleteNode = root
+        if (deleteNode == null) throw NullPointerException("impossible case") // (->3)
+
+        replaceNode = getNodeForReplace(deleteNode)
+        replaceNode?.let {
+            replaceNode = unLink(it, getParentByValue(it.value))
+        } // if deleteNode doesn't have children => replaceNode = null
+
+        if (parentNode != null) rebaseNode(deleteNode, parentNode, replaceNode)
+        else root = rebaseNode(deleteNode, null, replaceNode)
+
+        return deleteNode
+    }
 
     protected abstract fun unLink(
         node: NodeType,
@@ -131,14 +187,19 @@ abstract class TreeStruct<Pack : Comparable<Pack>, NodeType : Node<Pack, NodeTyp
     ): NodeType
 
     protected abstract fun rebaseNode(
-        //TODO linkLeafAsNode - [написал какой-то кринж, надо хорошо подумать, как это делать]
         node: NodeType,
         parent: NodeType?,
         replaceNode: NodeType?,
     ): NodeType? // return linked on the node place (null - if the replaceNode == null...)
 
+    protected abstract fun linkNewNode(
+        node: NodeType,
+        parent: NodeType?,
+    ): NodeType
+
+    protected abstract fun createNode(item: Pack): NodeType
+
     fun find(obj: Pack): Pack? {
-        /* Behavior: if find(item) == true => replace value with item, otherwise create a new node */
         //TODO test - find
         if (findItem(obj) == null) return null
         else return obj
--- lib/src/main/kotlin/treelib/binTree/BINStruct.kt
+++ lib/src/main/kotlin/treelib/binTree/BINStruct.kt
@@ -1,3 +1,4 @@
+//TODO нужны ли let? везде
 package treelib.binTree
 
 import treelib.abstractTree.TreeStruct
@@ -13,19 +14,9 @@ class BINStruct<Pack : Comparable<Pack>> : TreeStruct<Pack, BINNode<Pack>>() {
         /*Behaviour: return - linked replaceNode*/
         //TODO: rebaseNode - test
         if (replaceNode != null) {
-            replaceNode.left = node.left
-            node.left = null
-            replaceNode.right = node.right
-            node.right = null
-        }
-        if (parent != null){
-            when{
-                node.value inRightOf parent -> parent.right = replaceNode
-                node.value inLeftOf parent -> parent.left = replaceNode
-                else -> return null
-            }
-        }
-        return replaceNode
+            node.value = replaceNode.value
+            return node
+        } else return null
     }
 
     override fun unLink(
@@ -33,7 +24,7 @@ class BINStruct<Pack : Comparable<Pack>> : TreeStruct<Pack, BINNode<Pack>>() {
         parent: BINNode<Pack>?
     ): BINNode<Pack> {
         /*Behaviour:
-        * 1) must be used with one or two children == null
+        * 1) must be used with [one or two] children == null
         * 2) return - Node without children */
         /*TODO: unLink - test*/
         val unLinkedNode: BINNode<Pack> = node
@@ -41,73 +32,36 @@ class BINStruct<Pack : Comparable<Pack>> : TreeStruct<Pack, BINNode<Pack>>() {
 
         when {
             (node.right != null) && (node.left != null) -> return node //means-error (in correct node input)
-            (node.right != null) -> childForLink = node.right
-            (node.left != null) -> childForLink = node.left
+            node.right != null -> childForLink = node.right
+            node.left != null -> childForLink = node.left
             else -> childForLink = null
         }
         unLinkedNode.left = null
         unLinkedNode.right = null
-        if (parent != null) {
-            when {
-                (node.value < parent.value) -> parent.right = childForLink
-                (node.value > parent.value) -> parent.left = childForLink
-            }
+        if (parent == null) return unLinkedNode
+        when {
+            (node.value < parent.value) -> parent.right = childForLink
+            (node.value > parent.value) -> parent.left = childForLink
         }
         return unLinkedNode
     }
 
+    override fun linkNewNode(
+        //TODO make an abstract?
+        node: BINNode<Pack>,
+        parent: BINNode<Pack>?,
+    ): BINNode<Pack> {
+        if (parent == null) root = node
+        else {
+            if (node.value > parent.value) parent.right = node
+            else parent.left = node
+        }
+        return node
+    }
+
+    override fun createNode(item: Pack) = BINNode(item)
+
     override fun delete(item: Pack): Pack? = deleteItem(item)?.value
 
-    override fun deleteItem(item: Pack): BINNode<Pack>? {
-        /*return: null - error; value - deleted value*/
-        /*TODO: test - deleteItem [невозможные ветки: - проверить, что они реально невозможны
-           (->1 & 2) => что невозможно достичь deleteNode = parentNode?. со значением null (потому что inRightOf/inLeftOf)
-           (->3) => странный кейс, когда удаляемое значение - null; такого вообще быть не должно
-           ] */
-        val parentNode: BINNode<Pack>? // parent node of the node for deleting
-        var replaceNode: BINNode<Pack>?   // node for relink on the place deleted node
-        var deleteNode: BINNode<Pack>? = null // node for deleting
-
-        if (findItem(item) != null) {
-            parentNode = getParentByValue(item)
-            when {
-                parentNode == null -> deleteNode = root
-                item inRightOf parentNode -> deleteNode = parentNode.right //(->1)
-                item inLeftOf parentNode -> deleteNode = parentNode.left   //(->2)
-            }
-            if (deleteNode != null) {
-                replaceNode = getNodeForReplace(deleteNode)
-                if (replaceNode != null) {
-                    replaceNode = unLink(replaceNode, getParentByValue(replaceNode.value))
-                } else replaceNode = null // if deleteNode doesn't have children
-
-                if (parentNode == null) root = rebaseNode(deleteNode, parentNode, replaceNode)
-                else rebaseNode(deleteNode, parentNode, replaceNode)
-
-                return deleteNode
-            } else return null // (->3)
-        } else return null
-    }
-
     override fun insert(item: Pack): Pack = insertItem(item).value
-
-    override fun insertItem(item: Pack): BINNode<Pack> {
-        val parentNode: BINNode<Pack>?
-        val currentNode: BINNode<Pack>
-        val updateNode: BINNode<Pack>? = findItem(item)
-
-        if (updateNode == null) {
-            parentNode = getLeafForInsert(item)
-            currentNode = BINNode(value = item)
-            if (parentNode != null) {
-                if (item > parentNode.value) parentNode.right = currentNode
-                else parentNode.left = currentNode
-            } else root = currentNode
-
-            return currentNode
-        } else {
-            updateNode.value = item
-            return updateNode
-        }
-    }
 }
