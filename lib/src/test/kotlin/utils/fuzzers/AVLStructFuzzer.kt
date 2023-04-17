dbe672ac208b97998019e462221fd7aef73f13ef
Georgy S <it.is.mr.lag@gmail.com>
2023-04-17 20:56:33 +0300

    feat: Implement partial functionality of AVLStructFuzzer.

--- lib/src/test/kotlin/utils/fuzzers/AVLStructFuzzer.kt
+++ lib/src/test/kotlin/utils/fuzzers/AVLStructFuzzer.kt
@@ -1,7 +1,16 @@
 package utils.fuzzers
 
 import treelib.avlTree.AVLNode
+import treelib.avlTree.AVLStateContainer
+import treelib.avlTree.AVLStruct
+import utils.AVLAnalyzer
 
-class AVLStructFuzzer<Pack: Comparable<Pack>>(){
-//    TODO
+class AVLStructFuzzer<Pack : Comparable<Pack>>(
+    override val baseInput: Array<Pack>,
+    override val assertMethod: (input: String) -> Unit
+) : TreeStructFuzzer<Pack, AVLNode<Pack>, AVLAnalyzer<Pack>, AVLStateContainer<Pack>, AVLStruct<Pack>>() {
+
+    override fun createTreeStruct(): AVLStruct<Pack> = AVLStruct()
+
+    override fun createAnalyzer(): AVLAnalyzer<Pack> = AVLAnalyzer(assertMethod)
 }
