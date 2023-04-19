b9ef5e8788dc071a4308b090b56963fa7b03326d
Georgy S <it.is.mr.lag@gmail.com>
2023-04-19 03:20:10 +0300

    feat: Implement AVLStruct.restoreStruct and BINStruct.restoreStruct.

---
+++ lib/src/main/kotlin/treelib/singleObjects/exceptions/IncorrectUsage.kt
@@ -0,0 +1,18 @@
+package treelib.singleObjects.exceptions
+
+class IncorrectUsage  : Exception {
+    constructor() : super(
+        "Incorrect use of the tree"
+    )
+
+    constructor(message: String) : super(
+        "$message",
+    )
+
+    constructor(message: String, cause: Throwable) : super(
+        "$message",
+        cause,
+    )
+
+    constructor(cause: Throwable) : super(cause)
+}
